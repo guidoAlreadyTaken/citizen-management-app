16fbe86ef459e330547997e430e7bcc4b9bdb248
guidoAlreadyTaken <chris.baer9105@gmail.com>
2018-04-03 15:16:08 +0200

    config.sample

--- src/app/config.sample.ts
+++ src/app/config.sample.ts
@@ -1,4 +1,6 @@
 // Copy this file to config.ts and fill in appropriate values.
 export const config = {
-  apiUrl: 'https://example.com/api'
+  apiUrl: 'https://example.com/api',
+  qimgUrl: 'https://comem-qimg.herokuapp.com/api',
+  qimgSecret: 'Ujy+yk918DjSOGI2LS8t5FLAROqBtXfjRE7xglb5BCFVFXi+pSphYLJFUmQqqqbIxRpTG4vibZtzgsbmGETKsn4JK8RQLFI+fOaZJHarDKiVJOflSdw3Dwp9CScM5PPC5i7yKcfpWnH61bQP78mnmUUKRlZqkw/KQAnlrl2Q3ZE='
 }
